729f28733e832a2d976e2fc2227259ece2df9e65
lnkdhorizon <nicolas.rojas.fu@usach.cl>
2023-08-03 13:53:25 -0400

    reversion 3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='DatAs',
+    name='DatAsPy',
     version='4.0',
     packages=['datos'],
     url='https://github.com/Sofi1423/DatAs',  # Cambia 'tu_usuario' por tu nombre de usuario de GitHub
     license='MIT',
-    author='Sofia Fredes, Josefina Navarrete, Nicolas Rojsa',
+    author='Sofia Fredes, Josefina Navarrete, Nicolas Rojas',
     description='libreria creada con el proposito de dividir imagenes FITS y encontrar contraste para ubicar puntos de interes',
     classifiers=[
         "License :: OSI Approved :: MIT License",
